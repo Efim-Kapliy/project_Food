2f6a616c94cc73097ac93488f941f38ee6139989
Efim-Kapliy <efim0424@mail.ru>
2023-07-27 18:06:45 +0300

    Creating tabs in a new project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,45 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
 
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Tabs
+
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsParent = document.querySelector(".tabheader__items"),
+    tabsContent = document.querySelectorAll(".tabcontent");
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach(item => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabContent();
+  showTabContent(0);
+  tabsParent.addEventListener("click", event => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
